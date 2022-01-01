1b8433df4cc284e333a3fcd436aa39ed7d22bd05
amarronin <96934315+amarronin@users.noreply.github.com>
2022-01-01 11:47:33 +0530

    Add files via upload

---
+++ Dataset opns.R
@@ -0,0 +1,28 @@
+#for inat dataset operations
+#Dataset operations:
+#Remove dups of species (keep latest by date)
+#Sort by taxon_kingdom_name taxon_phylum_name	taxon_family_name	taxon_genus_name scientific_name
+#loop will be by family, genus
+#page will have 2 obs. or 1 if thats tough
+#Will create HTML. Then move that as PDF
+
+dsetopn = function(csv_name)
+  
+{
+data <- read.csv("observations-206143.csv")
+da1 = data.frame(data)
+data$obs_date = as.Date(data$observed_on)
+#takes one obs by latest date
+da2 = da1 %>%
+  arrange(scientific_name,desc(id))%>%
+  arrange(taxon_family_name,taxon_genus_name,scientific_name) %>%
+  group_by(taxon_family_name,taxon_genus_name,scientific_name) %>%
+  slice(1)
+  
+
+  return(da2)
+}
+
+amt = c("observations-206143")
+dsetopn(csv_name = amt)
+
